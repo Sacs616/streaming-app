c1ec572cee1ad4993d1e71229b161e8b088ce363
Sebastián Capúz <capuz.sebastian@outlook.com>
2026-02-16 18:31:04 -0500

    feat: update

---
+++ frontend/src/environments/environment.prod.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    production: true,
+    apiUrl: 'https://api.netflix-clone.com/api/v1',
+    apiTimeout: 30000,
+    tokenKey: 'netflix_token',
+    refreshTokenKey: 'netflix_refresh_token',
+    userKey: 'netflix_user'
+};
